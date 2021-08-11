e4d39d7c36836932837f1d8ab7b7b2bbf7d10159
unknown <nemezio321@gmail.com>
2021-08-10 23:29:14 -0500

    criando o pageadmin e tpl-admin

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
-
+use \Hcode\PageAdmin;
 $app = new Slim();
 
 $app->config('debug', true);
@@ -17,6 +17,15 @@ $app->get('/', function() {
 	echo json_encode($results);*/
 
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
