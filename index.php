1ee043b89d1198b0d440f32c900879e3cedba2fa
unknown <nemezio321@gmail.com>
2021-08-10 22:21:55 -0500

    removendo os arquivos de configuração

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php 
 
 require_once("vendor/autoload.php");
+use \Slim\Slim;
+use \Hcode\Page;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
@@ -10,9 +12,13 @@ $app->get('/', function() {
     
 	//echo "OK";
 
-	$sql = new Hcode\DB\Sql();
+	/*$sql = new Hcode\DB\Sql();
 	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
+	echo json_encode($results);*/
+
+	$page = new Page();
+
+	$page->setTpl("index");
 
 });
 
