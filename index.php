aa871f3e042d46db6338ea0dfa659a2794e701e7
unknown <nemezio321@gmail.com>
2021-08-12 23:04:47 -0500

    Implementando tela de login

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php 
 
+session_start();
+
 require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+
+
 $app = new Slim();
 
 $app->config('debug', true);
@@ -23,7 +28,8 @@ $app->get('/', function() {
 });
 
 $app->get('/admin', function() {
-    
+	
+	User::verifyLogin();
 
 	$page = new PageAdmin();
 
@@ -31,6 +37,35 @@ $app->get('/admin', function() {
 
 });
 
+$app-> get('/admin/login', function(){
+	
+	$page= new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	
+	$page-> setTpl("login");
+
+});
+
+$app-> post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+});
+
+
+$app-> get('/admin/logout', function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+
+});
+
 $app->run();
 
- ?>
+?>
--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -9,6 +9,8 @@ class Page{
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=> true,
+        "footer"=>true,
         "data" => []
     ];
 
@@ -28,7 +30,7 @@ class Page{
         
         $this->setdata($this->options["data"]);
 
-        $this->tpl->draw("header");
+       if($this->options["header"] === true) $this->tpl->draw("header");
     }
 
     private function setData($data = array()){
@@ -43,7 +45,7 @@ class Page{
     }
 
     public function __destruct(){
-        $this->tpl->draw("footer");
+       if($this->options["footer"] === true) $this->tpl->draw("footer");
     }
 }
 
